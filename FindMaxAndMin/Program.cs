7c36f467dd57f16467aaeea1485748ad32d8cffd
KALEEM IQBAL <olduser0071@gmail.com>
2024-09-20 20:29:17 +0530

    updated loop

--- FindMaxAndMin/Program.cs
+++ FindMaxAndMin/Program.cs
@@ -2,7 +2,7 @@
 string inputAsArray = Console.ReadLine();
 int[] arr = Array.ConvertAll(inputAsArray.Split(' '), int.Parse);
 int max = arr[0], min = arr[0];
-for (int i = 0; i < arr.Length - 1; i++)
+for (int i = 0; i < arr.Length; i++)
 {
     if (max < arr[i]) max = arr[i];
     if (min > arr[i]) min = arr[i];
